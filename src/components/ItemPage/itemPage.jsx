3c31dcf5c9ac7ab3629d0dd3e1ac1aa945744473
Bhirawa Mbani <bhirmbani@gmail.com>
2017-10-13 08:34:31 +0700

    input bind in constructor

--- src/components/ItemPage/itemPage.jsx
+++ src/components/ItemPage/itemPage.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import mockData from "../../mock/api.json";
 import ItemDetails from "../ItemDetails/itemDetails";
 
-const ItemPage = () => (
-  <div>
-    {mockData.items.map(item => (
-      <ItemDetails key={item.id} item={item} />
-    ))}
-  </div>
-);
+class ItemPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: ""
+    };
+
+    this.handleSearchInput = this.handleSearchInput.bind(this);
+  }
+
+  handleSearchInput(e) {
+    this.setState({ searchInput: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.handleSearchInput}
+          value={this.state.searchInput}
+          type="text"
+          placeholder="Search..."
+        />
+        {mockData.items.map(item => <ItemDetails key={item.id} item={item} />)}
+      </div>
+    );
+  }
+}
 
 export default ItemPage;
